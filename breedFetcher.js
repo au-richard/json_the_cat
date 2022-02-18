c449e30a9b153c88904ebc4d82c5829f8df41779
Richard <au.richard4@gmail.com>
2022-02-17 16:55:59 -0800

    Created breed fetcher to check if url has the cat breed

---
+++ breedFetcher.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const request = require("request");
+
+const breedType = process.argv[2];
+const url = `https://api.thecatapi.com/v1/breed/search?q=${breedType}`;
+
+request(url, (error, response, body) => {
+  // console.log(typeof body);
+
+  // console.log('error:', error);
+  // console.log('statusCode:', response && response.statusCode);
+  // console.log('body:', body);
+
+
+  const data = JSON.parse(body);
+  if (data.length === 0) {
+    console.log("Breed Not Found");
+  }
+
+  if (!response.ok) {
+    console.log("Error 404: URL Not Found");
+  }
+
+  if (data.length >= 1) {
+    // console.log(data);
+    // console.log(typeof data);
+    console.log(data[0].description);
+  }
+});
