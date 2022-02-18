c61793df4650cd37a1c96d96ed00fa87ea0639df
Richard <au.richard4@gmail.com>
2022-02-17 20:48:28 -0800

    Created Test File to test breedFetcher conditions

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,28 +1,27 @@
 const request = require("request");
 
-const breedType = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedType}`;
 
-request(url, (error, response, body) => {
-  // console.log(typeof body);
+const fetchBreedDescription = function (breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
+    const data = JSON.parse(body);
+    //If Breed is Not Found
+    if (data.length === 0) {
+      callback(null, "Breed Not Found");
+    }
+    //If URL is not correct
 
-  // console.log('error:', error);
-  // console.log('statusCode:', response && response.statusCode);
-  // console.log('body:', body);
+    //If Breed Exists
+    if (data.length >= 1) {
+      // console.log(data);
+      // console.log(typeof data);
+      callback(null, data[0].description);
+    }
+  });
+};
 
 
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("Breed Not Found");
-  }
 
-  if (!response.ok) {
-    console.log("Error 404: URL Not Found");
-  }
-
-  if (data.length >= 1) {
-    // console.log(data);
-    // console.log(typeof data);
-    console.log(data[0].description);
-  }
-});
+module.exports = { fetchBreedDescription };
